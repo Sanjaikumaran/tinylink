f18e62c45a95e7942fd69b2e1e195727e6c47f7a
Sanjaikumaran P <sanjaikumaran@makoitlab.com>
2025-11-23 01:31:50 +0530

    Server

--- app/src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-
-export const api = axios.create({
-  baseURL: API_BASE,
-});
--- app/src/types/types.ts
+++
@@ -1,7 +0,0 @@
-export type LinkType = {
-  code: string;
-  target_url: string;
-  total_clicks: number;
-  last_clicked: string;
-  created_at: string;
-};
